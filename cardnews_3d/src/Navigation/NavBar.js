eef0b21695b0a6408ddb048050ac8e09e7c0adce
Seoungwoo Baek <br1992ay@gmail.com>
2021-08-18 14:52:01 +0900

    REFACTOR: Responsible Navigation

--- cardnews_3d/src/Navigation/NavBar.js
+++ cardnews_3d/src/Navigation/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../scss/NavBar.scss';
 import logo from '../CardNews3D_logo.svg';
-
+import hamberger from '../Menus.svg';
 
 const NavBar = () => {
 
@@ -13,6 +13,7 @@ const NavBar = () => {
 		<div className = "NavBar-Template">
 			<div className = "Wrapper">
 				<img className = "Logo" onClick={handleClick} src={logo} alt = "logo"></img>
+				<img className = "Menus-pictogram" src={hamberger} alt="hamberger"></img>
 				<div className="Menu">
 					<div>Headline</div>
 					<div>Science</div>
