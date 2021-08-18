3555f4f56308e20e7b367a89e5fab6ebedeb5280
Seoungwoo Baek <br1992ay@gmail.com>
2021-08-18 15:44:43 +0900

    REFACTOR: Responsible View

--- cardnews_3d/src/articles/Article.js
+++ cardnews_3d/src/articles/Article.js
@@ -45,10 +45,10 @@ const Article = ({ data, sync }) => {
 		<div className="Article-template">
 			{/*<section className="card-section-one">*/}
 				<div className="one" data-onevisible={data.Article_one}>
-				<div className="wrapper">
-					<div className="haeder">카드뉴스 </div>
-					<div><button className="sync-btns" onClick={() => sync(!data.Sync)}>Sync</button></div>
-				</div>
+					<div className="wrapper">
+						<div className="haeder">카드뉴스 </div>
+						<div><button className="sync-btns" onClick={() => sync(!data.Sync)}>Sync</button></div>
+					</div>
 					<div className="body">
 						generator should succeed. This generator should be used and succeed. We will not go. Try hard to achieve everything you can succeed. We are extra cool everyday dancing. We are extra cool everyday dancing. This generator should be used and succeed. Try hard to achieve everything We will not try to bow and go. We are dancing.
 					</div>
